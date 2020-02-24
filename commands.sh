4b72b49977589aba013c3909aec07d32e39d7e82
Jason Morgan <jmorgan@f9vs.com>
2020-02-24 08:45:01 -0500

    Added tools.

--- commands.sh
+++ commands.sh
@@ -2,7 +2,7 @@
 
 ## Prep the vm
 sudo apt update
-sudo apt install jq -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt install jq -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common socat conntrack ipset
 sudo apt-get remove docker docker-engine docker.io containerd runc
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
@@ -284,3 +284,27 @@ subjects:
     kind: User
     name: kubernetes
 EOF
+
+## Worker prep
+
+wget -q --show-progress --https-only --timestamping \
+  https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.17.0/crictl-v1.17.0-linux-amd64.tar.gz \
+  https://github.com/opencontainers/runc/releases/download/v1.0.0-rc8/runc.amd64 \
+  https://github.com/containernetworking/plugins/releases/download/v0.8.2/cni-plugins-linux-amd64-v0.8.2.tgz \
+  https://github.com/containerd/containerd/releases/download/v1.2.9/containerd-1.2.9.linux-amd64.tar.gz \
+  https://storage.googleapis.com/kubernetes-release/release/v1.17.3/bin/linux/amd64/kubectl \
+  https://storage.googleapis.com/kubernetes-release/release/v1.17.3/bin/linux/amd64/kube-proxy \
+  https://storage.googleapis.com/kubernetes-release/release/v1.17.3/bin/linux/amd64/kubelet
+
+tar -xvf crictl-v1.17.0-linux-amd64.tar.gz
+
+mkdir containerd
+tar -xvf crictl-v1.17.0-linux-amd64.tar.gz
+tar -xvf containerd-1.2.9.linux-amd64.tar.gz -C containerd
+sudo tar -xvf cni-plugins-linux-amd64-v0.8.2.tgz -C /opt/cni/bin/
+sudo mv runc.amd64 runc
+chmod +x crictl kubectl kube-proxy kubelet runc 
+sudo mv crictl kubectl kube-proxy kubelet runc /usr/local/bin/
+sudo mv containerd/bin/* /bin/
+
+POD_CIDR=192.168.2.0/24
